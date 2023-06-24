196bd67991f7d5f2fbd077c15c12166dbb4b27be
root <root@localhost.localdomain>
2023-06-25 00:00:01 +0300

    automatic backup

--- backup.sh
+++ backup.sh
@@ -2,11 +2,11 @@
 cd /srv/samba/laila_201911092
 git add .
 git commit -m "automatic backup"
-git push origin master >> " /var/log/backup.log" 2>&1
+git push origin master >> "/var/log/backup.log" 2>&1
 if [ $? -eq 0 ]
 then 
-echo "backup successfull" >>" /var/log/backup.log"
+echo "backup successfull" >> "/var/log/backup.log"
 else 
-echo "backup failed">>" /var/log/backup.log"
+echo "backup failed" >> "/var/log/backup.log"
 fi 
 #end 
