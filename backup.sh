eba85db2865b3198dca5621deabf5cb7ab1f8dfa
root <root@localhost.localdomain>
2023-06-24 23:58:57 +0300

    automatic backup

--- backup.sh
+++ backup.sh
@@ -5,8 +5,8 @@ git commit -m "automatic backup"
 git push origin master >> " /var/log/backup.log" 2>&1
 if [ $? -eq 0 ]
 then 
-echo "backup successfull" >> " /var/log/backup.log"
+echo "backup successfull" >>" /var/log/backup.log"
 else 
-echo "backup failed">> " /var/log/backup.log"
+echo "backup failed">>" /var/log/backup.log"
 fi 
 #end 
